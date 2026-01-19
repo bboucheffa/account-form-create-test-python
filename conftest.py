e23f6446dd8012645880c599d6b38894cc517117
Boucheffa <boucheffa_badis@outlook.com>
2026-01-19 10:32:47 +0100

    test3

--- conftest.py
+++ conftest.py
@@ -14,7 +14,7 @@ def driver():
     chrome_options.add_argument("--window-size=1920,1080")
 
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     #driver.maximize_window()
     yield driver
     driver.quit()
